9bf9d91575ec532d7c7be2d5c37b3b82e7471c3c
Zach-Allen <zach.allen2814@gmail.com>
2021-01-20 19:50:35 -0700

    Added books

--- books.php
+++ books.php
@@ -1,5 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-echo '{"books-read":[{"Title":"The Hobbit","Author":"J. R. R. Tolkien"},{"Title":"The Body", "Author":"Bill Bryson"}]}'
+echo '{"books-read":[{"Title":"Shoe Dog","Author":"Phil Knight"},{"Title":"How to Destroy America In Three Easy Steps", "Author":"Ben Shapiro"}]}'
 ?>
