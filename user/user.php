b9393e6091a30bdbacf2515bb3f5b0c9b0eb4718
Zach-Allen <zach.allen2814@gmail.com>
2021-02-04 18:45:56 -0700

    Adding user page stuff

---
+++ user/user.php
@@ -0,0 +1,13 @@
+<?php
+
+  Class User{
+    public function getUser($_user_id){
+      return "";
+    }
+
+    public function getUserName(){
+      return "Zach";
+    }
+  }
+
+?>
---
+++ user_view.php
@@ -0,0 +1,12 @@
+<?php
+  require_once('./header.php');
+?>
+
+<?php
+  require_once('./user/user.php');
+
+  $user = new user();
+  $user->getUser(1);
+
+  echo $user->getUsername();
+?>
