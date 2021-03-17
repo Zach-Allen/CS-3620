bbd609c8c353f4a11e772006e9f204effe9c4950
Zach-Allen <zach.allen2814@gmail.com>
2021-03-17 10:41:24 -0600

    Update create_show.php

--- create_show.php
+++ create_show.php
@@ -22,11 +22,6 @@
         color: blue;
         font-size: 20px;
     }
-    
-    .buttonStyle {
-        width: 75px;
-        margin: auto;
-    }
 </style>
 
 <title>Add Show</title>
@@ -36,11 +31,13 @@
             <div>Show name:<input class="w3-input" type="text" name="show_name" onfocus="this.value=''" /></div>
             <div>Show Rating:<input class="w3-input" type="text" name="show_rating" onfocus="this.value=''" /></div>
             <div>Show description:<input class="w3-input" type="text" name="show_description" onfocus="this.value=''" /></div>
-            <input 
-                class="btn btn-primary w3-button w3-round w3-blue buttonStyle" 
-                type="submit"
-                value="Create Show" 
-            />
+            <center>
+                <input 
+                    class="btn btn-primary w3-button w3-round w3-blue buttonStyle" 
+                    type="submit"
+                    value="Create Show" 
+                />
+            </center>
 </form>
 
 
