2e0699bb7fdf56c47c1d6383debcacf7acdf208c
Zach-Allen <zach.allen2814@gmail.com>
2021-03-16 23:05:00 -0600

    UI changes

--- header.php
+++ header.php
@@ -1,3 +1,51 @@
-<h1>
-This is the header!
-</h1>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  main {
+    background-color: gray;
+    width: 100%;
+    height: 25px;
+  }
+
+  div {
+    text-align: center;
+    font-size: 20px;
+  }
+
+  th {
+    text-align: left;
+  }
+
+  a {
+    color: blue;
+    font-size: 20px;
+  }
+
+  .cardContainer {
+    width: 90%;
+    margin: 0 auto;
+    padding: 0.5em 0;
+    display: flex;
+    flex-flow: wrap;
+
+  }
+
+  .cards {
+    width: 90%;
+    margin: 0.5em auto;
+    flex: 1 0 24%;
+    margin: 1%;
+  }
+
+  .card {
+    /*flex: 0 1 24%;*/
+    
+  }
+</style>
+
+<main>
+  
+</main>
