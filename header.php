064dd5a647dc26beee4912ebc7bd98dd674c85ee
Zach-Allen <zach.allen2814@gmail.com>
2021-03-16 23:14:25 -0600

    UI stuff

--- header.php
+++ header.php
@@ -1,3 +1,48 @@
-<h1>
-This is the header!
-</h1>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+
+  .nav {
+    padding: 6px;
+    color: white;
+    background-color: gray;
+    margin-top: -10px;
+    font-size: 20px;
+    height: 75px;
+  }
+
+  th {
+    text-align: left;
+  }
+
+  a {
+    color: blue;
+    font-size: 20px;
+  }
+
+  .cardContainer {
+    width: 90%;
+    margin: 0 auto;
+    padding: 0.5em 0;
+    display: flex;
+    flex-flow: wrap;
+
+  }
+
+  .cards {
+    width: 90%;
+    margin: 0.5em auto;
+    flex: 1 0 24%;
+    margin: 1%;
+  }
+
+  .card {
+    /*flex: 0 1 24%;*/
+    
+  }
+</style>
+
+<main>
+  <div class="nav">
+    <h1>Show List</h1>
+  </div>
+</main>
---
+++ show/showDAO.php
@@ -0,0 +1,52 @@
+<?php
+class showDAO {
+  function getAllShows(){
+    require_once('./utilities/connection.php');
+    require_once('./show/show.php');
+
+    $sql = "SELECT show_id, show_name, show_rating, show_description, user_id FROM cs3620_proj.shows";
+    $result = $conn->query($sql);
+
+    $shows;
+    $index = 0;
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $show = new show();
+
+            $show->setShowId($row["show_id"]);
+            $show->setShowName($row["show_name"]);
+            $show->setShowRating($row["show_rating"]);
+            $show->setShowDescription($row["show_description"]);
+            $show->setUserId($row["user_id"]);
+            $shows[$index] = $show;
+            $index = $index + 1;
+        }
+    }
+    else {
+        echo "0 results";
+    } 
+    $conn->close();
+
+    return $shows;
+  }
+
+  function createShow($show){
+    require_once('./utilities/connection.php');
+
+    $insertShow = $conn->prepare("INSERT INTO cs3620_proj.shows (`show_name`,
+    `show_rating`, `show_description`) VALUES (?, ?, ?)");
+
+    $sn = $show->getShowName();
+    $sr = $show->getShowRating();
+    $sd = $show->getShowDescription();
+
+    $insertShow->bind_param("sss", $sn, $sr, $sd);
+    $insertShow->execute();
+
+    $insertShow->close();
+    $conn->close();
+  }
+}
+?>
